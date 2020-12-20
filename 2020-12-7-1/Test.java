183c7692da96a7937c550b7a3d9c6662800a6f53
Your Name <you@example.com>
2020-12-20 13:29:11 +0800

    输出不同数据类型的数据

---
+++ 2020-12-7-1/Test.java
@@ -0,0 +1,22 @@
+public class Test{
+	public static void main(String args[]){
+		float price=88.8F;
+		
+		int a='C';
+
+		double height=1.88F;
+		
+
+		float price2=(float)88.8;
+		
+		System.out.println(a);
+
+		System.out.println(price2);
+
+		System.out.println("hihi");
+
+		char c1='\u6797';
+
+		System.out.println(c1);
+	}
+}
